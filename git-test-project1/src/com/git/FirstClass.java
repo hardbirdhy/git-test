79dc741164707903e5b1544efd9b2c05ed6974da
hardbirdhy <997321432@qq.com>
2021-08-18 21:49:47 +0800

    把本地工程添加到git

---
+++ git-test-project1/src/com/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.git;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+
+}
